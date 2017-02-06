e4bd1bfc0cdec76fd79d0a3f7e2e9f56720233ea
andyleap <andyleap@gmail.com>
2017-02-06 09:25:50 -0500

    updates for protocol and bot server

--- client.go
+++ client.go
@@ -21,11 +21,14 @@ type Client struct {
 func Connect(server string, userid string, username string) (*Client, error) {
 
 	dialer := &websocket.Dialer{}
-	dialer.EnableCompression = true
+	dialer.EnableCompression = false
 	url := "ws://ws.generals.io/socket.io/?EIO=3&transport=websocket"
 
 	if server == "eu" {
 		url = "ws://euws.generals.io/socket.io/?EIO=3&transport=websocket"
+	}
+	if server == "bot" {
+		url = "ws://botws.generals.io/socket.io/?EIO=3&transport=websocket"
 	}
 
 	c, _, err := dialer.Dial(url, nil)
@@ -50,21 +53,26 @@ func (c *Client) Run() error {
 		}
 	}()
 	go func() {
+		time.Sleep(100 * time.Millisecond)
 		for data := range c.sendChan {
 			err := c.c.WriteMessage(websocket.TextMessage, data)
+			//log.Println("Sending: ", string(data))
 			if err != nil {
 				c.c.Close()
 			}
 		}
 	}()
+	c.sendMessage("set_username", c.userID, c.username)
 	for {
 		_, message, err := c.c.ReadMessage()
 		if err != nil {
 			return err
 		}
+		//log.Println("Got: ", string(message))
 		dec := json.NewDecoder(bytes.NewBuffer(message))
 		var msgType int
 		dec.Decode(&msgType)
+
 		if msgType == 42 {
 			var raw json.RawMessage
 			dec.Decode(&raw)
@@ -84,29 +92,73 @@ func (c *Client) sendMessage(v ...interface{}) {
 	c.sendChan <- newbuf
 }
 
+func (c *Client) Close() {
+	c.c.Close()
+}
+
 func (c *Client) JoinCustomGame(ID string) *Game {
-	c.sendMessage("join_private", ID, c.username, c.userID)
-	time.Sleep(50 * time.Millisecond)
-	c.sendMessage("set_username", c.userID, c.username)
+	c.sendMessage("join_private", ID, c.userID)
 	g := &Game{c: c, ID: ID}
 	g.registerEvents()
 	return g
 }
 
+func (c *Client) Join1v1() *Game {
+	c.sendMessage("join_1v1", c.userID)
+	g := &Game{c: c, ID: "1v1"}
+	g.registerEvents()
+	return g
+}
+
 func (c *Client) JoinClassic() *Game {
-	c.sendMessage("play", c.username, c.userID)
-	time.Sleep(50 * time.Millisecond)
-	c.sendMessage("set_username", c.userID, c.username)
+	c.sendMessage("play", c.userID)
 	g := &Game{c: c}
 	g.registerEvents()
 	return g
 }
 
 func (c *Client) JoinTeam(team string) *Game {
-	c.sendMessage("join_team", team, c.username, c.userID)
-	time.Sleep(50 * time.Millisecond)
-	c.sendMessage("set_username", c.userID, c.username)
-	g := &Game{c: c}
+	c.sendMessage("join_team", team, c.userID)
+	g := &Game{c: c, ID: "2v2"}
 	g.registerEvents()
 	return g
+}
+
+type Ranking struct {
+	Name  string  `json:"name"`
+	Stars float64 `json:"stars"`
+}
+
+type Replay struct {
+	Type    string    `json:"type"`
+	ID      string    `json:"id"`
+	Started int64     `json:"started"`
+	Turns   int       `json:"turns"`
+	Ranking []Ranking `json:"ranking"`
+}
+
+func (c *Client) GetReplaysForUser(userID string) []Replay {
+	c.sendMessage("replay_list", struct {
+		UserID string `json:"user_id"`
+	}{UserID: userID})
+	reply := make(chan []Replay)
+	c.events["replay_list"] = func(data json.RawMessage) {
+		replays := []Replay{}
+		decode := []interface{}{nil, &replays}
+		json.Unmarshal(data, &decode)
+		reply <- replays
+	}
+	return <-reply
+}
+
+func (c *Client) GetReplays() []Replay {
+	c.sendMessage("replay_list", map[string]interface{}{})
+	reply := make(chan []Replay)
+	c.events["replay_list"] = func(data json.RawMessage) {
+		replays := []Replay{}
+		decode := []interface{}{nil, &replays}
+		json.Unmarshal(data, &decode)
+		reply <- replays
+	}
+	return <-reply
 }
